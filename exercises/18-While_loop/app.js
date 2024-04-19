40f9af9324984a46744359cd20f31be1f944139e
AngelikaWebDev <110480648+AngelikaWebDev@users.noreply.github.com>
2024-04-20 00:50:08 +0200

    Update app.js

--- exercises/18-While_loop/app.js
+++ exercises/18-While_loop/app.js
@@ -1,11 +1,9 @@
 //fix this function:
 function startCounting() {
 	let counter = 100;
-	while (counter <= 100) {
+	while (counter > 0) {
 		counter--;
-		console.log(counter);
 	}
-
 	return counter;
 }
 
