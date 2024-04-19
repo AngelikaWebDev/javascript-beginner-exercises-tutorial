dd011f76ed21e2ee7243d70626473b9e94b84d50
AngelikaWebDev <110480648+AngelikaWebDev@users.noreply.github.com>
2024-04-20 00:47:02 +0200

    Update app.js

--- exercises/15-Rand_from_one_to_six/app.js
+++ exercises/15-Rand_from_one_to_six/app.js
@@ -1,6 +1,8 @@
-function getRandomInt()
+function getRandomInt(min, max)
 {
-	let randomNumber = Math.random();
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
 	return randomNumber;
 }
-console.log(getRandomInt());
+console.log(getRandomInt(1,6));  
