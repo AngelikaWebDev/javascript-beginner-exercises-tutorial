ac18b15c7f1c67c278857e8eea5113e576eb7bc1
AngelikaWebDev <110480648+AngelikaWebDev@users.noreply.github.com>
2024-04-20 00:51:57 +0200

    Update app.js

--- exercises/22-The_Beatles/app.js
+++ exercises/22-The_Beatles/app.js
@@ -1,3 +1,20 @@
+function sing() {
+    let line = "let it be";
+    let answer = "there will be an answer";
+    let wisdom = "whisper words of wisdom";
+
+    let lyrics = "";
+    for (let i = 0; i < 4; i++) {
+        lyrics += line + ", ";
+    }
+    lyrics += answer + ", ";
+    for (let i = 0; i < 4; i++) {
+        lyrics += line + ", ";
+    }
+    lyrics += wisdom + ", " + line;
+    
+    return lyrics;
+}
 
 
 //Your code above ^^^
